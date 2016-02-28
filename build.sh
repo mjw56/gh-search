692c1aa87da8c8f678c0702c41152094d3548c36
Mike Wilcox <mwilcox56@gmail.com>
2016-02-28 03:58:47 -0500

    modify build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,5 @@ rm -rf dist-module/
 mkdir dist/
 mkdir dist-module/
 
-babel index.js \
-  --out-file dist/index.js
-
 babel src \
   --out-dir dist/src
-
-webpack \
-  --config webpack.dist.config.js
